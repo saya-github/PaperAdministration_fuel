79a5efec0e79f87d032f4a367d7baaaa3e8b5aad
saya <sayaka.shimada.mail.g@gmail.com>
2015-10-29 14:44:12 +0900

    update
    views update

--- views/paperadministration/parts/content.php
+++ views/paperadministration/parts/content.php
@@ -6,15 +6,18 @@
         echo '<h1>'.$pagetitle.'</h1>';
     }
     foreach($papers as $paper) {
-        echo '<p></p>
+        echo '<div class="paper-fluid">
+            <p></p>
             <h3>'.$paper['title'].'</h3>
+            <p></p>
             <li>著者：'.$paper['author'].'</li>
             <li>bibtexID：'.$paper['bibtexID'].'</li>
             <li>学年：'.$paper['degree'].'</li>
             <li>年：'.$paper['year'].'</li>
             <li>分野：'.$paper['category'].'</li>
             <li>ファイル名：'.$paper['filename'].'</li>
-            <p></p>';
+            <p></p>
+            </div>';
     }
 
 ?>
--- views/paperadministration/parts/newpaper.php
+++ views/paperadministration/parts/newpaper.php
@@ -2,6 +2,7 @@
   <div class="row">
     <div class="col-lg-12">
       <h1>What's new</h1>
+      <p></p>
 <?php
     foreach($papers as $paper) {
         echo '<li>'.$paper['title'].'：'.$paper['author'].'</li>';
